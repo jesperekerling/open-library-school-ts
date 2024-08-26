e02f9159cf3f56d4394b47cfbd43cffd6aad5c3d
Jesper Ekerling <jesper@forme.se>
2024-08-27 00:14:54 +0200

    Footer edited

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 function Footer() {
   return (
-    <footer className='container mx-auto text-center py-5'>
-        © 2024 <a href="https://ekerling.com" target='blank'>ekerling.com</a>
+    <footer className='container mx-auto text-center py-5 text-sm'>
+        <p className='py-2'><a href="https://github.com/jesperekerling/open-library-school-ts" target='blank' className='hover:opacity-80'>Code for this project at Github</a> | Powered by <a href="https://openlibrary.org" target='blank' className='hover:opacity-80'>Open Library</a></p>
+        © 2024 <a href="https://ekerling.com" target='blank' className='hover:opacity-80'>ekerling.com</a>
     </footer>
   )
 }
